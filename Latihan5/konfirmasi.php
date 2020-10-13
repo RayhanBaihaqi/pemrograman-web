09c8b114b8747d98dadc2e81d5188ea640cf96e0
RayhanBaihaqi <baihaqir78@gmail.com>
2020-10-13 12:25:48 +0700

    di perbaharui

--- Latihan5/konfirmasi.php
+++ Latihan5/konfirmasi.php
@@ -1,4 +1,3 @@
-  
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,11 +7,24 @@
 </head>
 <body>
 <?php
+    include_once "koneksi.php";
     if (isset($_POST['tombolSubmit'])) {
         $username = $_POST['username'];
+        $password = $_POST['password'];
         $email = $_POST['email'];
         $namaDepan = $_POST['namaDepan'];
         $namaBelakang = $_POST['namaBelakang'];
+
+        $sql = "INSERT INTO registrasi (username, password, email, namaDepan, namaBelakang)
+        VALUES ('$username','$password','$email','$namaDepan','$namaBelakang')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "Data berhasil diinput";
+        }
+        else {
+            echo "Data tidak berhasil diinput pada string sql: <br>
+            $sql <br> dg error: ".mysqli_error($conn);
+        }
     
 ?>
     username: <?php echo $username ?>
